a446b9d8ab514fd7d82c81972451bba4d612b15f
Alexandre <alexandre.guerrier@isen-ouest.yncrea.fr>
2025-10-07 14:29:47 +0200

    Ajout fichier projet

---
+++ src/main/java/fr/isencaen/api_what_time/ApiWhatTimeApplication.java
@@ -0,0 +1,13 @@
+package fr.isencaen.api_what_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWhatTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWhatTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/isencaen/api_what_time/ApiWhatTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.isencaen.api_what_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWhatTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
