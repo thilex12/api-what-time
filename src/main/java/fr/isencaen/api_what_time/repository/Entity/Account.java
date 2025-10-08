d836e3ace6e74f64b18a49116678081e9920e749
Alexandre <alexandre.guerrier@isen-ouest.yncrea.fr>
2025-10-09 01:11:33 +0200

    Tags et Inscri dans account

--- src/main/java/fr/isencaen/api_what_time/repository/Entity/Account.java
+++ src/main/java/fr/isencaen/api_what_time/repository/Entity/Account.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name="account")
+@Table(name = "account")
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,37 +23,81 @@ public class Account {
     private List<Tag> tags;
 
 
-    public Account(){}
-    public Account(int id, String name, String surname, String mail, String pwd){
+    public Account() {
+    }
+
+    public Account(int id, String name, String surname, String mail, String pwd) {
         this.id = id;
         this.name = name;
         this.surname = surname;
         this.mail = mail;
         this.pwd = pwd;
     }
-    public Account(String name, String surname, String mail, String pwd){
+
+    public Account(String name, String surname, String mail, String pwd) {
         this.name = name;
         this.surname = surname;
         this.mail = mail;
         this.pwd = pwd;
     }
 
-    public int getId(){return id;}
-    public String getName(){return name;}
-    public String getSurname(){return surname;}
-    public String getMail(){return mail;}
-    public String getPwd(){return pwd;}
+    public int getId() {
+        return id;
+    }
 
-    public void setName(String name){
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
-    public void setSurname(String surname){
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
         this.surname = surname;
     }
-    public void setMail(String mail){
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
         this.mail = mail;
     }
-    public void setPwd(String pwd){
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
         this.pwd = pwd;
+    }
+
+    public List<Allow> getAllowedList() {
+        return allowedList;
+    }
+
+    public void setAllowedList(List<Allow> allowedList) {
+        this.allowedList = allowedList;
+    }
+
+    public List<Inscription> getInscriptions() {
+        return inscriptions;
+    }
+
+    public void setInscriptions(List<Inscription> inscriptions) {
+        this.inscriptions = inscriptions;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
     }
 }
