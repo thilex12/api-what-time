fbcc2f80c623d0e70bdcc7edf4560143b84b8fee
Gezoo64 <peyrachearnaud@gmail.com>
2025-10-12 13:33:54 +0200

    Tests en lien avec Account complets et fonctionnels

--- src/main/java/fr/isencaen/api_what_time/service/AccountService.java
+++ src/main/java/fr/isencaen/api_what_time/service/AccountService.java
@@ -34,18 +34,22 @@ public class AccountService {
     @Autowired
     private FollowRepository followRepository;
 
-    private String reformatStrEntry(String s){
+    public String reformatStrEntry(String s, boolean lower){
         if (s == null) return null;
-        return s.toLowerCase().strip();
+        if (lower) return s.toLowerCase().strip();
+        return s.strip();
+    }
+    public String reformatStrEntry(String s){ // mail doit correspondre au format donné par reformatStrEntry
+        return reformatStrEntry(s, false);
     }
 
-    private boolean mailInvalid(String mail){
+    public boolean mailInvalid(String mail){ // mail doit correspondre au format donné par reformatStrEntry
         if (mail == null) return true;
         // Source du regex : https://www.developpez.net/forums/d220837/java/general-java/langage/verifier-validite-d-adresse-email/
         return !mail.matches(".+@.+\\.[a-z]+");
     }
 
-    private boolean mailUsed(String mail){ // mail doit correspondre au format donné par reformatStrEntry
+    public boolean mailUsed(String mail){ // mail doit correspondre au format donné par reformatStrEntry
         if (mail == null) return true;
         List<AccountModel> checkMail = getAccountModelByEmail(mail);
         return !checkMail.isEmpty();
@@ -57,16 +61,19 @@ public class AccountService {
         this.followRepository = followRepository;
     }
 
+    // Récupère le compte de l'utilisateur connecté
     public Account getUserAccount(){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (!(auth.isAuthenticated() && auth.getPrincipal() instanceof AccountPrincipal user)) return null;
         return user.getAccount();
     }
 
+    // Génère un AccountModel à partir du compte de l'utilisateur connecté
     public AccountModel getUserModel(){
         return AccountModel.of(getUserAccount());
     }
 
+    // Récupère le compte d'un utilisateur donné (via ID)
     public AccountModel getAccountModelById(int id){
         try{
             return accountRepository.findById(id).stream().map(AccountModel::of).toList().getFirst();
@@ -76,6 +83,8 @@ public class AccountService {
         }
     }
 
+    // Récupère une liste de compte suivant un mail donné
+    // Si tout est ok dans notre contexte, la liste ne contiendra que 0 ou 1 élément
     public List<AccountModel> getAccountModelByEmail(String mail){
         try{
             return accountRepository.findByMail(mail).stream().map(AccountModel::of).toList();
@@ -85,11 +94,12 @@ public class AccountService {
         }
     }
 
+    // Créé un compte à partir d'un model donné
     public AccountModel createAccount(CreateAccountModel createAccountModel){
         String name, surname, mail, pwd;
         name = reformatStrEntry(createAccountModel.name());
         surname = reformatStrEntry(createAccountModel.surname());
-        mail = reformatStrEntry(createAccountModel.mail());
+        mail = reformatStrEntry(createAccountModel.mail(), true);
         pwd = createAccountModel.pwd(); // Ne doit pas être formaté...
 
         if (name == null || surname == null || mail == null || pwd == null){
@@ -110,6 +120,50 @@ public class AccountService {
         );
     }
 
+    // Mets à jour le suivi des tags d'un compte donné (issue du repository) à partir d'une liste d'id de tags
+    @Transactional
+    public void updateTags(Account bddAccount, List<Integer> tags){
+        // On liste tous les suivis de tag de l'utilisateur
+        List<FollowTag> followTagsInBDD = followRepository.findAllByAccount(bddAccount);
+
+        // On en fait une liste de tags
+        List<Tag> followTags = new ArrayList<>();
+        for(FollowTag ft : followTagsInBDD){
+            followTags.add(ft.getTag());
+        }
+
+        // Pour chacun des tags passés en paramètre de la requête
+        for(int id : tags){
+            try { // Si l'id courant existe :
+                // On récupère le tag réel
+                Tag tag = tagRepository.findById(id).orElseThrow();
+
+                // On regarde si l'utilisateur suit déjà ce tag
+                if (!followTags.contains(tag)){ // Si non, maintenant il le suit
+                    followRepository.save(new FollowTag(tag, bddAccount));
+                }
+
+                // On supprime le tag de la liste de ceux identifiés précedement
+                followTags.remove(tag);
+
+            }
+            catch (NoSuchElementException e){}
+        }
+
+        // On itère sur chacun des tags identifiés comme suivis, qui n'ont pas été trouvés dans la liste en paramètre
+        for (Tag t : followTags){
+            // Pour chacun, on identifie l'enregistrement du follow correspondant
+            for(FollowTag ft : followTagsInBDD){
+                if (t == ft.getTag()){
+                    // On supprime la ligne en mettant à jour account (dans removeFollow)
+                    ft.removeFollow();
+                    followRepository.delete(ft);
+                }
+            }
+        }
+    }
+
+    // Mets à jour les informations du compte de l'utilisateur connecté, à partir d'un model groupant les changements
     @Transactional
     public AccountModel updateAccount(UpdateAccountModel updateAsked){
         // Récupération de la bdd de la ligne de l'utilisateur connecté
@@ -129,7 +183,7 @@ public class AccountService {
         List<Integer> tags;
         name = reformatStrEntry(updateAsked.name());
         surname = reformatStrEntry(updateAsked.surname());
-        mail = reformatStrEntry(updateAsked.mail());
+        mail = reformatStrEntry(updateAsked.mail(), true);
         pwd = updateAsked.pwd(); // Ne pas formatter le mdp
         tags = updateAsked.tags();
 
@@ -146,51 +200,11 @@ public class AccountService {
         }
 
         // Si un champ est demandé à être modifié, on le modifie
-        if (name != null && !name.isBlank()) bddAccount.setName(updateAsked.name());
-        if (surname != null && !surname.isBlank()) bddAccount.setSurname(updateAsked.surname());
-        if (mail != null && !mail.isBlank()) bddAccount.setMail(updateAsked.mail());
-        if (pwd != null && !pwd.isBlank()) bddAccount.setPwd(bCryptPasswordEncoder.encode(updateAsked.pwd()));
-        if (tags != null){
-            // On liste tous les suivis de tag de l'utilisateur
-            List<FollowTag> followTagsInBDD = followRepository.findAllByAccount(bddAccount);
-
-            // On en fait une liste de tags
-            List<Tag> followTags = new ArrayList<>();
-            for(FollowTag ft : followTagsInBDD){
-                followTags.add(ft.getTag());
-            }
-
-            // Pour chacun des tags passés en paramètre de la requête
-            for(int id : tags){
-                try { // Si l'id courant existe :
-                    // On récupère le tag réel
-                    Tag tag = tagRepository.findById(id).orElseThrow();
-
-                    // On regarde si l'utilisateur suit déjà ce tag
-                    if (!followTags.contains(tag)){ // Si non, maintenant il le suit
-                        followRepository.save(new FollowTag(tag, bddAccount));
-                    }
-
-                    // On supprime le tag de la liste de ceux identifiés précedement
-                    followTags.remove(tag);
-
-                }
-                catch (NoSuchElementException e){}
-            }
-
-            // On itère sur chacun des tags identifiés comme suivis, qui n'ont pas été trouvés dans la liste en paramètre
-            for (Tag t : followTags){
-                // Pour chacun, on identifie l'enregistrement du follow correspondant
-                for(FollowTag ft : followTagsInBDD){
-                    if (t == ft.getTag()){
-                        // On supprime la ligne en mettant à jour account (dans removeFollow)
-                        ft.removeFollow();
-                        followRepository.delete(ft);
-                    }
-                }
-            }
-
-        }
+        if (name != null && !name.isBlank()) bddAccount.setName(name);
+        if (surname != null && !surname.isBlank()) bddAccount.setSurname(surname);
+        if (mail != null && !mail.isBlank()) bddAccount.setMail(mail);
+        if (pwd != null && !pwd.isBlank()) bddAccount.setPwd(bCryptPasswordEncoder.encode(pwd));
+        if (tags != null) updateTags(bddAccount, tags);
         return AccountModel.of(bddAccount);
     }
 
